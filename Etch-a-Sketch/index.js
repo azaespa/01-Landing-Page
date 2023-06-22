858d3cb48516d78e244873b1b8bfc44a35b477d1
azaespa <pauljamesazanes@gmail.com>
2023-06-23 00:03:18 +0800

    Interactive boxes

--- Etch-a-Sketch/index.js
+++ Etch-a-Sketch/index.js
@@ -1,1 +1,30 @@
-const gridContainer = document.querySelector(".grid-container");
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++)
+{
+    let row = document.createElement("div");
+    row.classList.add(`row`);
+    for (let j = 0; j < 16; j++)
+    {
+        let column = document.createElement("div");
+        column.classList.add(`col`);
+        row.appendChild(column);
+    }
+    container.appendChild(row);
+}
+
+const boxes = container.querySelectorAll(".col");
+
+function handleHover() {
+    if (!this.classList.contains("hovered")) {
+        this.classList.add("hovered");
+    }
+}
+
+function init() {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", handleHover);
+    }
+}
+
+init();
