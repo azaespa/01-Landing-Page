c5952b8fdd1a9f6b68e6001fb50e437d630e5b78
azaespa <pauljamesazanes@gmail.com>
2023-07-02 03:11:04 +0800

    Interactive digits, operators, and display.
    The calculator will display the result number and which operator have been used. The calculator wont let the user type 0 in the beginning of each digits. Operators will operate the computation. and Equals will calculate recursively.

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,14 +1,41 @@
+const actionTakenTxt = document.getElementById("action-taken-txt");
 const resultTxt = document.getElementById("result-txt");
 const digitElements = document.getElementsByClassName("digits");
 const operatorElements = document.getElementsByClassName("operators");
+const equalsBtn = document.getElementById("equals");
 
 const digits = [...digitElements];
 const operators = [...operatorElements];
 
 let isOperatorClicked = false;
+let isDigitClicked = true;
 
-let numbers = [0,0];
+let numbers = [];
+let addend = null;
 let operator = null;
+
+function updateActionTakenTxt(element) {
+    let op = element.innerText;
+    if (element.id == "equals") {
+        switch(operator) {
+            case "add":
+                op = "+";
+                break;
+            case "sub":
+                op = "-";
+                break;
+            case "mul":
+                op = "x";
+                break;
+            case "div":
+                op = "÷";
+                break;
+        }
+        actionTakenTxt.value = `${numbers[0]} ${op} ${numbers[1]} =`;
+        return;
+    }
+    actionTakenTxt.value = `${numbers[0]} ${op}`;
+}
 
 function updateResultTxt(innerText) {
     if (isOperatorClicked) {
@@ -16,7 +43,66 @@ function updateResultTxt(innerText) {
         isOperatorClicked = false;
     };
 
-    resultTxt.value += innerText
+    if (Number(resultTxt.value) == 0) {
+        resultTxt.value = "";
+    }
+
+    resultTxt.value += innerText;
+    isDigitClicked = true;
+}
+
+function operate(operatorId) {
+
+    const firstNumber = numbers[0];
+    let secondNumber = Number(resultTxt.value);
+
+    switch(operatorId) {
+        case "add":
+            resultTxt.value = firstNumber + secondNumber;
+            break;
+        case "sub":
+            resultTxt.value = firstNumber - secondNumber;
+            break;
+        case "mul":
+            resultTxt.value = firstNumber * secondNumber;
+            break;
+        case "div":
+            resultTxt.value = firstNumber / secondNumber;
+            break;
+    }
+
+    if (numbers.length > 1) {
+        numbers.push(Number(resultTxt.value));
+        numbers.shift();
+    } 
+}
+
+function calculate(operatorId) {
+
+    if (numbers.length == 1) numbers.push(Number(resultTxt.value));
+
+    let firstNumber = numbers[0];
+    let secondNumber = numbers[1];
+
+    switch(operatorId) {
+        case "add":
+            resultTxt.value = firstNumber + secondNumber;
+            break;
+        case "sub":
+            resultTxt.value = firstNumber - secondNumber;
+            break;
+        case "mul":
+            resultTxt.value = firstNumber * secondNumber;
+            break;
+        case "div":
+            resultTxt.value = firstNumber / secondNumber;
+            break;
+    }
+    
+    numbers[0] = Number(resultTxt.value);
+
+    console.log(numbers)
+
 }
 
 function digitsEventListener(){
@@ -29,17 +115,30 @@ function digitsEventListener(){
 function operatorsEventListener() {
     operators.map(element => 
         element.addEventListener("click", () => {
-            isOperatorClicked = true;
-            numbers.push(Number(resultTxt.value));
-            numbers.shift();
+            if (isDigitClicked) {
+                numbers.push(Number(resultTxt.value));
+                
+                if (numbers.length > 2) numbers.shift();
+                
+                operate(operator);
+            }
 
-            console.log(numbers);
+            isOperatorClicked = true;
+            isDigitClicked = false;
+            operator = element.id;
+            updateActionTakenTxt(element);
         }))
+}
+
+function handleEqualsBtn() {
+    calculate(operator)
+    updateActionTakenTxt(equalsBtn)
 }
 
 function init() {
     digitsEventListener();
     operatorsEventListener();
+    equalsBtn.addEventListener("click", handleEqualsBtn);
 }
 
 init();
