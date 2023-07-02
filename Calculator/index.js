d76c2b45907995997997383ebe2fde1dabf4b316
azaespa <pauljamesazanes@gmail.com>
2023-07-02 22:04:39 +0800

    Fully interactive calculator buttons
    AC button clears the result display. Plus minus button adds and removes negative sign to the display. Backspace button removes a digit. Digits button add a digit. Decimal button can only add one decimal per instance. Operators button operate their math. Equals button can calculate recursively

--- Calculator/index.js
+++ Calculator/index.js
@@ -3,11 +3,14 @@ const resultTxt = document.getElementById("result-txt");
 const digitElements = document.getElementsByClassName("digits");
 const operatorElements = document.getElementsByClassName("operators");
 const equalsBtn = document.getElementById("equals");
-const decimal = document.getElementById("decimal");
+const decimalBtn = document.getElementById("decimal");
 const clearBtn = document.getElementById("ac-btn");
+const backspaceBtn = document.getElementById("bs-btn");
+const plusMinusBtn = document.getElementById("pm-btn");
 
 const digits = [...digitElements];
 const operators = [...operatorElements];
+const MAX_DIGIT = 10;
 
 let isOperatorClicked = false;
 let isDigitClicked = true;
@@ -20,6 +23,7 @@ let operator = null;
 let veryLastNum = null;
 let counter = 0;
 let decimalCounter = 0;
+let fontSize = 16 * 4;
 
 function updateActionTakenTxt(element) {
     let op = element.innerText;
@@ -40,17 +44,30 @@ function updateActionTakenTxt(element) {
                 op = "÷";
                 break;
         }
-        actionTakenTxt.value = `${numbers[0]} ${op} ${numbers[1]} =`;
+        
+        actionTakenTxt.value = numbers[1] != undefined ? `${numbers[0]} ${op} ${numbers[1]} =` : `${numbers[0]} =`;
+
+        if (resultTxt.value == "BRUH") actionTakenTxt.value = "BRUH";
+
         return;
     }
 
     actionTakenTxt.value = `${num} ${op}`;
+    
+    if (resultTxt.value == "BRUH") actionTakenTxt.value = "BRUH";
+
 }
 
 function updateResultTxt(innerText) {
+    if (getResultTxtLength() >= MAX_DIGIT && !isOperatorClicked) return;
+    if (resultTxt.value == "Lol") clearAll();
+
     if (isOperatorClicked) {
         resultTxt.value = "";
         isOperatorClicked = false;
+        
+        fontSize = 16 * 4;
+        resultTxt.style.fontSize = `${fontSize}px`;
     };
 
     if (Number(resultTxt.value) == 0 && !resultTxt.value.includes(".")) {
@@ -59,10 +76,22 @@ function updateResultTxt(innerText) {
 
     if (innerText == "." && Number(resultTxt.value) == 0) {
         resultTxt.value = "0"
+
+        isEqualsClicked = false;
     }
 
     resultTxt.value += innerText;
     isDigitClicked = true;
+
+    if (resultTxt.scrollWidth > resultTxt.clientWidth) {
+        fontSize -= 8;
+        resultTxt.style.fontSize = `${fontSize}px`;
+    }
+}
+
+function getResultTxtLength() {
+    const symbols = [".", "-"]
+    return symbols.some(res => resultTxt.value.includes(res)) ? resultTxt.value.length - 1 : resultTxt.value.length;
 }
 
 function operate(operatorId) {
@@ -81,7 +110,7 @@ function operate(operatorId) {
             resultTxt.value = firstNumber * secondNumber;
             break;
         case "div":
-            resultTxt.value = firstNumber / secondNumber;
+            resultTxt.value = (firstNumber / secondNumber) == Infinity ? "BRUH" : firstNumber / secondNumber;
             break;
     }
 
@@ -90,6 +119,7 @@ function operate(operatorId) {
         numbers.shift();
         isLastActionOperator = true;
     } 
+
 }
 
 function calculate(operatorId) {
@@ -114,13 +144,20 @@ function calculate(operatorId) {
             resultTxt.value = firstNumber * secondNumber;
             break;
         case "div":
-            resultTxt.value = firstNumber / secondNumber;
+            resultTxt.value = (firstNumber / secondNumber) == Infinity ? "BRUH" : firstNumber / secondNumber;
             break;
     }
     
     numbers[0] = Number(resultTxt.value);
     numbers[1] = isLastActionOperator ? veryLastNum : numbers[1];
     isLastActionOperator = false;
+
+    if (resultTxt.scrollWidth > resultTxt.clientWidth) {
+        while (resultTxt.scrollWidth > resultTxt.clientWidth) {
+            fontSize -= 8;
+            resultTxt.style.fontSize = `${fontSize}px`;
+        }
+    }
 }
 
 function clearAll() {
@@ -130,8 +167,11 @@ function clearAll() {
     isOperatorClicked = false;
     isDigitClicked = true;
     isEqualsClicked = false;
+    numbers = [];
     counter = 0;
     decimalCounter = 0;
+    fontSize = 16 * 4;
+    resultTxt.style.fontSize = `${fontSize}px`;
 }
 
 function digitsEventListener(){
@@ -145,6 +185,8 @@ function digitsEventListener(){
 function operatorsEventListener() {
     operators.map(element => 
         element.addEventListener("click", () => {
+            if (resultTxt.value == "BRUH") clearAll();
+
             if (isDigitClicked) {
                 numbers.push(Number(resultTxt.value));
                 
@@ -162,22 +204,39 @@ function operatorsEventListener() {
 }
 
 function handleEqualsBtn() {
+    if (resultTxt.value == "BRUH") clearAll();
     calculate(operator)
     updateActionTakenTxt(equalsBtn)
     isEqualsClicked = true;
 }
 
-function handleDecimalBtn(event) {
+function handleDecimalBtn() {
+    if (isEqualsClicked) clearAll();
     decimalCounter++;
-    if (decimalCounter < 2) updateResultTxt(decimal.innerText);
+    if (decimalCounter < 2) updateResultTxt(decimalBtn.innerText);
+}
+
+function handleBackspaceBtn() {
+    if (resultTxt.value.length < 2) return resultTxt.value = "0";
+
+    let temp = resultTxt.value;
+    temp = temp.slice(-1 * temp.length, -1);
+    resultTxt.value = temp;
+}
+
+function handlePlusMinusBtn() {
+    let temp = Number(resultTxt.value) * -1;
+    resultTxt.value = temp;
 }
 
 function init() {
     digitsEventListener();
     operatorsEventListener();
     equalsBtn.addEventListener("click", handleEqualsBtn);
-    decimal.addEventListener("click", handleDecimalBtn);
+    decimalBtn.addEventListener("click", handleDecimalBtn);
     clearBtn.addEventListener("click", clearAll);
+    backspaceBtn.addEventListener("click", handleBackspaceBtn);
+    plusMinusBtn.addEventListener("click", handlePlusMinusBtn);
 }
 
 init();
