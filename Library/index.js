42b045ccd3415237d8e5b176babd5619d3fa8cb8
azaespa <pauljamesazanes@gmail.com>
2023-07-27 21:45:13 +0800

    Interactive form for adding a new book

--- Library/index.js
+++ Library/index.js
@@ -1,26 +1,78 @@
-const addNewBook = document.getElementById("add-new-book");
 const modal = document.querySelector("dialog");
+const content = document.querySelector(".content");
+const addNewBook = document.querySelector("#add-new-book");
+const formAddBook = document.querySelector("#form-add-book");
+
 const cardEntries = document.querySelectorAll(".card.entry");
-
-addNewBook.addEventListener("click", function() { modal.showModal() });
-
-for (let entry of cardEntries) {
-    entry.addEventListener("click", function() { 
-        document.querySelector(".selected").classList.remove("selected");
-        entry.classList.toggle("selected"); 
-    });
-}
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
+    this.createCard = function() {
+        const card = document.createElement("div");
+        card.classList.add("card", "entry");
+        
+        const cover = document.createElement("div");
+        const image = document.createElement("img");
+        cover.classList.add("cover");
+        image.src = "src/js-for-dummies.jpg";
+        image.alt = this.title;
+
+        const bookTitle = document.createElement("h3");
+        bookTitle.classList.add("book-title");
+        bookTitle.title = this.title;
+        bookTitle.innerText = this.title;
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.classList.add("book-author");
+        bookAuthor.innerText = this.author;
+
+        cover.append(image);
+        card.append(cover);
+        card.append(bookTitle);
+        card.append(bookAuthor);
+        content.append(card);
+
+        card.addEventListener("click", function() {
+            document.querySelector(".selected").classList.remove("selected");
+            card.classList.toggle("selected");             
+        })
     }
+}
+
+addNewBook.addEventListener("click", function() { 
+    modal.showModal(); 
+
+    const formInputTexts = document.querySelectorAll("#form-add-book > input:not(input[type='checkbox'])");
+    const formInputCheckbox = document.querySelector("#form-book-status")
+
+    for (input of formInputTexts) {
+        input.value = "";
+    }
+
+    formInputCheckbox.checked = false;
+});
+
+for (let entry of cardEntries) {
+    entry.addEventListener("click", function() {
+        document.querySelector(".selected").classList.remove("selected");
+        entry.classList.toggle("selected"); 
+    });
 }
 
 const theHobbit = new Book('The Hobbit', 'Bitoy', '11', 'not read yet');
 
-console.log(theHobbit.info());
+formAddBook.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const bookTitle = event.currentTarget.formBookTitle.value;
+    const bookAuthor = event.currentTarget.formBookAuthor.value;
+    const bookPages = event.currentTarget.formBookPages.value;
+    const bookStatus = event.currentTarget.formBookStatus.checked;
+    
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+    newBook.createCard();
+
+    modal.close();
+})
