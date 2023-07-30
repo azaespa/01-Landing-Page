f3032f7462dd665664b42f8399d57345a375d894
azaespa <pauljamesazanes@gmail.com>
2023-07-30 14:34:33 +0800

    Gameboard html

---
+++ Tic-tac-toe/index.js
@@ -0,0 +1,13 @@
+const Player = (sign) => {
+    return { sign }
+};
+
+const gameBoard = (() => {
+    
+})
+
+const gameController = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+
+})
