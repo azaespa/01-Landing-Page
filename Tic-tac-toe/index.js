e6de37b4bd9217410686d709354a4d731f352930
azaespa <pauljamesazanes@gmail.com>
2023-07-31 12:09:51 +0800

    gameController analyzeGame isGaveOver

--- Tic-tac-toe/index.js
+++ Tic-tac-toe/index.js
@@ -6,7 +6,6 @@ const gameBoard = (() => {
     const board = new Array(9);
     const setField = (index, sign) => {
         board[index] = sign;
-        console.log(board);
     }
     const getField = (index) => {
         return board[index];
@@ -18,15 +17,48 @@ const gameBoard = (() => {
 const gameController = (() => {
     const playerX = Player("X");
     const playerO = Player("O");
+    let currentPlayerSign = "";
     let round = 1;
+    let isGameOver = false;
 
     const getCurrentPlayerSign = () => {
-        const currentPlayerSign = (round % 2 == 1) ? playerX.sign : playerO.sign;
+        currentPlayerSign = (round % 2 == 1) ? playerX.sign : playerO.sign;
         round++;
         return currentPlayerSign;
     };
 
-    return { getCurrentPlayerSign };
+    const getOpponentPlayerSign = () => {
+        return (currentPlayerSign == playerX.sign) ? playerO.sign : playerX.sign;
+    }
+
+    const analyzeGame = () => {
+        const winningMoves = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ];
+
+        for (const winningMove of winningMoves) {
+            let playerMoves = [];
+            for (const winningMoveValue of winningMove.values()) {
+                playerMoves.push(gameBoard.board[winningMoveValue - 1]);
+            }
+            if (!playerMoves.includes(undefined)) {
+                if (!playerMoves.includes(getOpponentPlayerSign())) {
+                    console.log(playerMoves, winningMove);
+                    isGameOver = true;
+                }
+            }
+        }
+        console.log(`is game over: ${isGameOver}`)
+    } 
+
+    return { getCurrentPlayerSign, analyzeGame };
 })();
 
 const displayController = (() => {
@@ -37,6 +69,7 @@ const displayController = (() => {
             if (fields[i].textContent != "") return;
             gameBoard.setField(i, gameController.getCurrentPlayerSign());
             fields[i].textContent = gameBoard.getField(i);
+            gameController.analyzeGame();
         })
     }
 })();
